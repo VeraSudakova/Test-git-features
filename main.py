d5cc8c34f4569fb422e53bd33bf444863c8211a6
VeraSudakova <verxax@gmail.com>
2021-11-19 21:13:17 +0300

    Внесены изменения в main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from int_processing import int_process
 from string_processing import string_process
 
 
+a = 2
+b = 0
+c = 1
 ints = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 strings = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')
 res_int = int_process(ints)
